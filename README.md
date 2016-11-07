6391b1680140c87d0252308d0608b95970509226
Matt Griswold <grizz@20c.com>
2016-11-07 19:49:29 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+
+# cookiecutter-napalm-skeleton
+
+[Cookiecutter](https://cookiecutter.readthedocs.io/en/latest/) template for [napalm-skelecon](https://github.com/napalm-automation/napalm-skeleton).
+
+## Usage
+
+Make sure cookiecutter is installed:
+
+```sh
+pip install cookiecutter
+```
+
+Run the command, fill out the blanks:
+```sh
+cookiecutter cookiecutter-napalm-skeleton
+```
+
---
+++ {{cookiecutter.repo_name}}/setup.py
@@ -0,0 +1,31 @@
+"""setup.py file."""
+
+import uuid
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+__author__ = 'David Barroso <dbarrosop@dravetech.com>'
+
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+reqs = [str(ir.req) for ir in install_reqs]
+
+setup(
+    name="{{cookiecutter.package_name}}",
+    version="{{cookiecutter.version}}",
+    packages=find_packages(),
+    author="{{cookiecutter.author}}",
+    author_email="{{cookiecutter.author_email}}",
+    description="{{cookiecutter.short_description}}",
+    classifiers=[
+        'Topic :: Utilities',
+         'Programming Language :: Python',
+         'Programming Language :: Python :: 2',
+         'Programming Language :: Python :: 2.7',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+    ],
+    url="https://github.com/{{cookiecutter.github_owner}}/{{cookiecutter.repo_name}}",
+    include_package_data=True,
+    install_requires=reqs,
+)
